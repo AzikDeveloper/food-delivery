8085221837376d278446a1b1593c0c85eab5133c
AzikDeveloper <78961300+AzikDeveloper@users.noreply.github.com>
2021-12-08 14:45:28 +0500

    hyp

--- api/models.py
+++ api/models.py
@@ -14,7 +14,6 @@ class Product(models.Model):
     description = models.TextField(max_length=300, null=True)
     price = models.IntegerField(null=True)
     category = models.ManyToManyField(Category, related_name='products')
-    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
