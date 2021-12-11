796b96ab9cf690c862451846dca57eba85b49a7e
AzikDeveloper <78961300+AzikDeveloper@users.noreply.github.com>
2021-12-12 00:57:57 +0500

    ad

--- site_auth/models.py
+++ site_auth/models.py
@@ -11,15 +11,15 @@ class User(AbstractUser):
 class SMSCode(models.Model):
     phone_number = models.CharField(max_length=20, null=True)
     code = models.IntegerField(null=True, unique=True)
-    expiration_date = models.DateTimeField(null=True, default=datetime.today() + timedelta(minutes=2))
+    created_at = models.DateTimeField(null=True, auto_now_add=True)
 
     def __str__(self):
         return f'{self.phone_number}'
 
     def is_expired(self):
         today = datetime.today()
-        if self.expiration_date:
-            if self.expiration_date >= today:
+        if self.created_at:
+            if (today - self.created_at).total_seconds() <= 3 * 60:
                 return False
             else:
                 return True
