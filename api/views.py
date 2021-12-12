38d70a6377da0f2778d1ebd25afcdfc8625f545d
AzikDeveloper <78961300+AzikDeveloper@users.noreply.github.com>
2021-12-12 10:14:26 +0500

    ad

--- api/views.py
+++ api/views.py
@@ -26,8 +26,6 @@ def homeView(request):
 
 @api_view(['GET'])
 def productsView(request):
-    page = 1
-    per_page = 200
     order_list = []
     products = Product.objects.all()
 
@@ -36,42 +34,14 @@ def productsView(request):
     if 'sort' in query:
         for attr in query['sort'].split(','):
             order_list.append(attr.replace('_', '__'))
-    if 'page' in query:
-        if query['page'].isdigit():
-            page = int(query['page'])
-    if 'per_page' in query:
-        if query['per_page'].isdigit():
-            per_page = int(query['per_page'])
-
     try:
         products = products.order_by(*order_list)
     except FieldError:
         return Response(data={'detail': 'failed to process &sort= parameter'}, status=400)
-    paginator = Paginator(products, per_page)
-    num_pages = paginator.num_pages
 
-    if page <= num_pages:
-        products = paginator.page(page)
-    else:
-        page = num_pages
-        products = paginator.page(num_pages)
+    serializer = ProductSerializer(products, many=True)
 
-    product_serializer = ProductSerializer(products, many=True)
-
-    data = {
-        'pagination': {
-            'page': page,
-            'per_page': per_page,
-            'num_pages': num_pages
-        },
-        'products': product_serializer.data
-    }
-    if 'exclude' in query:
-        for attr in query['exclude'].split(','):
-            if attr in data:
-                data.pop(attr)
-
-    return Response(data=data)
+    return Response(data=serializer.data)
 
 
 @api_view(['GET'])
