f4349bffa907c54b50e0190b999af0992a3a2839
AzikDeveloper <78961300+AzikDeveloper@users.noreply.github.com>
2021-12-19 19:39:07 +0500

    a

--- api/tools.py
+++ api/tools.py
@@ -5,6 +5,8 @@ account_sid = "ACa1ea63f1a96512f69d5b667950f3a692"
 auth_token = "e34b0e652bee60228895a121d0aad9cd"
 from_phone_number = "+12182923493"
 
+send_sms = False
+
 
 class SMS:
     def __init__(self, to_phone_number):
@@ -15,9 +17,10 @@ class SMS:
         self.code = randint(100000, 999999)
 
     def send_sms(self):
-        client = Client(account_sid, auth_token)
-        client.messages.create(
-            to=self.to_phone_number,
-            from_=from_phone_number,
-            body=f'Food Delivery {self.code}'
-        )
+        if send_sms:
+            client = Client(account_sid, auth_token)
+            client.messages.create(
+                to=self.to_phone_number,
+                from_=from_phone_number,
+                body=f'Food Delivery {self.code}'
+            )
