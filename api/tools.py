849683fb5c5d5c8de3a975af201ca743c8004427
Azizbek Xushnazarov <a.khushnazarov@uic.grou>
2022-04-15 12:36:06 +0500

    fix: security

--- api/tools.py
+++ api/tools.py
@@ -1,5 +1,6 @@
 from twilio.rest import Client
 from random import randint
+from django.conf import settings
 
 
 class SMS:
@@ -11,10 +12,10 @@ class SMS:
         self.code = randint(100000, 999999)
 
     def send_sms(self):
-        if send_sms:
-            client = Client(account_sid, auth_token)
+        if settings.TWILIO_SEND_SMS:
+            client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
             client.messages.create(
                 to=self.to_phone_number,
-                from_=from_phone_number,
+                from_=settings.TWILIO_FROM_NUMBER,
                 body=f'Food Delivery {self.code}'
             )
